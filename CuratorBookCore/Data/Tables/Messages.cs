0e4ba82b2766bd3f4f3a8904af84e41cc504bbf2
Maxim Lukoshko <www.maxim.by97@gmail.com>
2017-11-23 20:38:48 +0300

    Messages table

---
+++ CuratorBookCore/Data/Tables/Messages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CuratorBookCore.Data.Tables
+{
+    public class Messages
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required]
+        public int SenderId { get; set; }
+
+        [ForeignKey("SenderId")]
+        public Users Sender { get; set; }
+
+
+        public int ReceiverId { get; set; }
+
+        [ForeignKey("ReceiverId")]
+        public Users Receiver { get; set; }
+
+
+        public int ReceiverGroupId { get; set; }
+
+        [ForeignKey("ReceiverGroupId")]
+        public Groups ReceiverGroup { get; set; }
+
+
+        public string Text { get; set; }
+
+
+        public DateTime SentDate { get; set; }
+
+
+        public bool NotifyViaEmail { get; set; }
+    }
+}
