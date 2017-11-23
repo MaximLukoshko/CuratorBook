05cac471496981ab057abf2cd6dd0c3597c3fd33
Maxim Lukoshko <www.maxim.by97@gmail.com>
2017-11-23 20:24:34 +0300

    UsersRights

---
+++ CuratorBookCore/Data/Tables/UsersRights.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CuratorBookCore.Data.Tables
+{
+    public class UsersRights
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        private Users User { get; set; }
+
+        public int RightId { get; set; }
+
+        [ForeignKey("RightId")]
+        public Rights Right { get; set; }
+    }
+}
