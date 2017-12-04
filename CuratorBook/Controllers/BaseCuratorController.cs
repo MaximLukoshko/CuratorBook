f02be3a29545c3b3f3a4e8f20b2d8eae9ab34c63
Maxim Lukoshko <www.maxim.by97@gmail.com>
2017-12-04 21:49:56 +0300

    Fixed missed reference

---
+++ CuratorBook/Controllers/BaseCuratorController.cs
@@ -0,0 +1,10 @@
+﻿using CuratorBookCore.Contract.MainService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuratorBook.Controllers
+{
+    public class BaseCuratorController : Controller
+    {
+        protected IMainService Service = new MainService();
+    }
+}
